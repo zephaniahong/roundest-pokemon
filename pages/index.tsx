3ae963e6ee4315dbbde300bd0482b4943e821aa4
Zephaniah Ong <zealorez@gmail.com>
2022-09-15 23:11:12 +0800

    Create basic UI structure for main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl bg-blue-400">hello</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="mb-2 text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="border rounded-md p-8 flex justify-between max-w-3xl items-center">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
