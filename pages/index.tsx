c050041dcc3d53edcd770e8ec1bc45eeab6b6d4b
Zephaniah Ong <zealorez@gmail.com>
2022-09-16 09:59:57 +0800

    Install and setup trpc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from "next";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "world" }]);
+  if (isLoading) return <div>...Loading...</div>;
+  if (data) return <div>{data.greeting}</div>;
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="mb-2 text-2xl text-center">Which Pokemon is rounder?</div>
