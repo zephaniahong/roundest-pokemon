a5d5624c618537a7c460004245f11268351357d8
Zephaniah Ong <zealorez@gmail.com>
2022-09-16 14:07:50 +0800

    Make votes persist

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,14 @@ const Home: NextPage = () => {
 
   const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
   const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+  const voteMutation = trpc.useMutation(["cast-vote"]);
 
   const voteForRoundest = (selected: number) => {
+    if (selected === first) {
+      voteMutation.mutate({ votedFor: first, votedAgainst: second });
+    } else {
+      voteMutation.mutate({ votedFor: second, votedAgainst: first });
+    }
     updateIds(getOptionsForVote());
   };
   const isPokemonReady =
